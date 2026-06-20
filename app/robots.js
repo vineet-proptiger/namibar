776a70258673d8b3ba39e9572c2c390faaf078b0
vineet-proptiger <vineet.mishra@proptiger.com>
2026-06-20 17:48:28 +0530

    ddddddd

--- app/robots.js
+++ app/robots.js
@@ -1,11 +1,11 @@
-// export default function robots() {
-//   return {
-//     rules: {
-//       userAgent: '*',
-//       allow: '/',
-//       disallow: '',
-//     },
-//     sitemap: 'https://prestigepalmcourtmahalaxmi.in/sitemap.xml',
-//   }
-// }
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '',
+    },
+    sitemap: 'http://nambiardistrict25sarjapur.co.in/sitemap.xml',
+  }
+}
 
--- app/sitemap.js
+++ app/sitemap.js
@@ -1,18 +1,10 @@
-// export default function sitemap() {
-//   const base = 'https://prestigepalmcourtmahalaxmi.in'
-
-//   return [
-//     {
-//       url: base + '/',
-//       lastModified: new Date(),
-//       changeFrequency: 'weekly',
-//       priority: 1.0,
-//     },
-//     {
-//       url: base + '/privacy-policy',
-//       lastModified: new Date(),
-//       changeFrequency: 'monthly',
-//       priority: 0.8,
-//     },
-//   ]
-// }
+      priority: 1.0,
+    },
+    {
+      url: base + '/privacy-policy',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
